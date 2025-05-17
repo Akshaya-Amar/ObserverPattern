1c3c2e9b18baddcf9a05f2dde55adf6793a33960
akshayaamar <akshayaamar05@gmail.com>
2025-05-18 02:53:56 +0530

    refactor: moved lifecycle observer logic in MyLiveData class

--- app/src/main/java/com/amar/observerpattern/MainActivity.kt
+++ app/src/main/java/com/amar/observerpattern/MainActivity.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(), LifecycleEventObserver {
+class MainActivity : AppCompatActivity() {
 
      private val values = MyLiveData<String>()
 
@@ -27,11 +24,11 @@ class MainActivity : AppCompatActivity(), LifecycleEventObserver {
                startActivity(Intent(this@MainActivity, MainActivity2::class.java))
           }
 
-          values.observe {
+          values.observe(this) {
                textView.text = it
           }
 
-          values.observe {
+          values.observe(this) {
                Log.d("testing...", "onCreate: observing value $it")
           }
 
@@ -39,61 +36,8 @@ class MainActivity : AppCompatActivity(), LifecycleEventObserver {
                for (i in 1..10) {
                     delay(2000)
                     Log.d("testing....", "onCreate: setting value $i")
-                    values.setValue("Updating value to $i", this@MainActivity)
+                    values.setValue("Updating value to $i")
                }
           }
-
-
-          lifecycle.addObserver(this)
-
-          Log.d("current state...", "onCreate: ${lifecycle.currentState}")
-     }
-
-     override fun onPause() {
-          super.onPause()
-          Log.d("current state...", "onPause: ${lifecycle.currentState}")
-     }
-
-     override fun onStart() {
-          super.onStart()
-          Log.d("current state...", "onStart: ${lifecycle.currentState}")
-     }
-
-     override fun onResume() {
-          super.onResume()
-          Log.d("current state...", "onResume: ${lifecycle.currentState}")
-     }
-
-     override fun onStop() {
-          super.onStop()
-          Log.d("current state...", "onStop: ${lifecycle.currentState}")
-     }
-
-     override fun onDestroy() {
-          super.onDestroy()
-          Log.d("current state...", "onDestroy: ${lifecycle.currentState}")
-     }
-
-     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-          var currentState = lifecycle.currentState
-          if (currentState == Lifecycle.State.DESTROYED) {
-               // remove observers
-               values.removeObservers()
-               Log.d("current state...1", "onStateChanged: destroyed")
-               return
-          }
-
-          var prevState: Lifecycle.State? = null
-
-          while (prevState != currentState) {
-               prevState = currentState
-//               activeStateChanged(isActive())
-               currentState = lifecycle.currentState
-          }
-          Log.d("current state...1", "onStateChanged: event - >$event")
-     }
-
-     private fun isActive(): Boolean {
-          return lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
      }
 }
--- app/src/main/java/com/amar/observerpattern/MyLiveData.kt
+++ app/src/main/java/com/amar/observerpattern/MyLiveData.kt
@@ -1,27 +1,33 @@
 package com.amar.observerpattern
 
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 
 class MyLiveData<T> {
-
-     private val observers = mutableListOf<MyObserver<T>>()
+     private val observers = mutableMapOf<MyObserver<T>, LifecycleBoundObserver<T>>()
      private var data: T? = null
 
-     fun observe(observer: MyObserver<T>) {
-          observers.add(observer)
+     fun observe(owner: LifecycleOwner, observer: MyObserver<T>) {
+          if (owner.lifecycle.currentState == Lifecycle.State.DESTROYED) return
+
+          val wrapper = LifecycleBoundObserver(owner, this)
+          val observerExistence = observers.putIfAbsent(observer, wrapper)
+          if (observerExistence != null) return
+          owner.lifecycle.addObserver(wrapper)
      }
 
-     fun setValue(value: T, owner: LifecycleOwner) {
+     fun setValue(value: T) {
           data = value
-          if (owner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-               dispatchValue()
-          }
+          dispatchValue()
      }
 
      private fun dispatchValue() {
-          observers.forEach { myObserver ->
-               notify(myObserver)
+          observers.forEach { (observer, wrapper) ->
+               val currentState = wrapper.owner.lifecycle.currentState
+               if (currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                    notify(observer)
+               }
           }
      }
 
@@ -31,7 +37,28 @@ class MyLiveData<T> {
           }
      }
 
-     fun removeObservers() {
-          observers.clear()
+     private fun removeObservers(owner: LifecycleOwner) {
+          val iterator = observers.iterator() // used iterator to avoid ConcurrentModificationException
+          while (iterator.hasNext()) {
+               val entry = iterator.next()
+               val wrapper = entry.value
+               if (wrapper.owner == owner) { // The same LiveData instance inside the SharedViewModel is observed by different fragments — each fragment is a different LifecycleOwner.
+                    iterator.remove()
+                    owner.lifecycle.removeObserver(wrapper)
+               }
+          }
+     }
+
+     class LifecycleBoundObserver<T>(
+          val owner: LifecycleOwner,
+          private val liveData: MyLiveData<T>
+     ) : LifecycleEventObserver {
+          override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+               val currentState = owner.lifecycle.currentState
+               if (currentState == Lifecycle.State.DESTROYED) {
+                    liveData.removeObservers(owner)
+                    return
+               }
+          }
      }
 }
